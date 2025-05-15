8999a7cdf0258ff371c03563d1ee13344bb8af68
Johan <johanruma@gmail.com>
2025-05-15 16:15:04 -0500

    Formato hojas de vida

---
+++ hojasDeVida.py
@@ -0,0 +1,42 @@
+emails = set()
+
+CVs = {("document"):
+       {
+           "personalInfo":
+           {
+            "name":"Daniel",
+           "phoneNumber":321432543,
+           "address":"CR 54",
+           "email":"k@l.com",
+           "birthAge":(2000)
+           },
+
+           "academicEductaion":[
+               ("institute","title","years")
+           ],
+
+            "profesionalExperience":[
+                ("company","job","functions","length")
+            ],
+
+            "personalReferences":[
+                ("nombre","relation","phoneNumber")
+            ],
+
+            "workReferences":[
+                ("nombre","relation","phoneNumber")
+            ],
+
+            "skills": set(
+                ""
+            ),
+
+            "certifications": set(
+                ""
+            )
+
+       }}
+
+
+# def add_cv():
+
